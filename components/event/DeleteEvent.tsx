9e3ae0fe1e7a78ea404db7fd64b6c5ed9cd0fa5d
kebzin <kebbawaiga@gmail.com>
2025-01-18 09:46:37 +0000

    Delete event

---
+++ components/event/DeleteEvent.tsx
@@ -0,0 +1,80 @@
+import React from "react";
+import {
+  AlertDialog,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { Trash, TriangleAlert } from "lucide-react";
+import { Label } from "../ui/label";
+import { Input } from "../ui/input";
+import { Alert, AlertDescription, AlertTitle } from "../ui/alert";
+import { Button } from "../ui/button";
+
+const DeleteEvent = ({ event }) => {
+  return (
+    <div>
+      <AlertDialog>
+        <AlertDialogTrigger asChild>
+          <Button variant="outline" className="w-full flex justify-between">
+            Delete <Trash className="text-destructive" />
+          </Button>
+        </AlertDialogTrigger>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>
+              <span className="text-destructive">
+                <TriangleAlert
+                  className="mr-1 inline-block stroke-destructive"
+                  size={18}
+                />{" "}
+                Delete Event
+              </span>
+            </AlertDialogTitle>
+            <AlertDialogDescription>
+              <p className="mb-2">
+                Are you sure you want to delete{" "}
+                <span className="font-bold">{event?.title}</span>?
+                <br />
+                This action will permanently remove this event with the it
+                realated data
+                {/* <span className="font-bold">
+                {currentRow.role.toUpperCase()}
+              </span>{" "} */}
+                from the system. This cannot be undone.
+              </p>
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+
+          <Label className="my-2">
+            Event Number:
+            <Input
+              className="mt-2"
+              //   value={value}
+              //   onChange={(e) => setValue(e.target.value)}
+              placeholder="Enter event number to confirm deletion."
+            />
+          </Label>
+
+          <Alert variant="destructive">
+            <AlertTitle>Warning!</AlertTitle>
+            <AlertDescription>
+              Please be carefull, this operation can not be rolled back.
+            </AlertDescription>
+          </Alert>
+
+          <AlertDialogFooter>
+            <AlertDialogCancel>Cancell</AlertDialogCancel>
+            <Button>Submits</Button>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+    </div>
+  );
+};
+
+export default DeleteEvent;
