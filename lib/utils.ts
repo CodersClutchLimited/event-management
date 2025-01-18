b51c8b0e27e84bde4c22081bc1c0f8e4acf65f29
kebzin <kebbawaiga@gmail.com>
2025-01-18 09:49:03 +0000

    added format iso date to readable date in utitls

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,25 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function formatReadableDate(dateString: string): string | undefined {
+  const date = new Date(dateString);
+
+  // Check if the date is invalid
+  if (isNaN(date.getTime())) {
+    throw new Error("Invalid date format");
+  }
+
+  // Return a formatted date with time (hours and minutes)
+  return date.toLocaleString("en-US", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+    hour12: true, // This will display the time in 12-hour format with AM/PM
+  });
 }
