c96ba9b187c6538bdcd4125b236d2843964ae615
kebzin <kebbawaiga@gmail.com>
2025-01-22 11:38:43 +0000

    Replace LoginForm with SignInForm in the authentication page

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,27 +1,11 @@
-"use client";
-import { LoginForm } from "@/components/login-form";
-// import { signUpWithCredentials } from "@/lib/actions/auth/signup-with-credentials";
-// async function onSubmit() {
-//     // console.log(values)
-//     // setError("");
-//     // setSuccess("");
+import SignInForm from "@/components/login-form";
 
-//       signUpWithCredentials(values)
-//         .then((data) => {
-//           if (data?.error) {
-//             setError(data.error);
-//           } else if (data?.success) {
-//             setSuccess(data.success);
-//           }
-//         })
-//         .catch(() => setError("Something went wrong"));
-//     );
 //   }
 export default function LoginPage() {
   return (
     <div className="flex min-h-svh flex-col items-center justify-center bg-muted p-6 md:p-10">
       <div className="w-full max-w-sm md:max-w-3xl">
-        <LoginForm />
+        <SignInForm />
       </div>
     </div>
   );
