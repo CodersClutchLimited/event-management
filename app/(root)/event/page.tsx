baa636ff81cbf248d0435dcb183757819652b42c
kebzin <kebbawaiga@gmail.com>
2025-01-22 11:37:44 +0000

    Fetch and display events with pagination and search functionality

--- app/(root)/event/page.tsx
+++ app/(root)/event/page.tsx
@@ -1,10 +1,48 @@
 import EventContainer from "@/components/event/EventContainer";
+import { GetAllEvent } from "@/lib/actions/event/GetAllEvent";
 import React from "react";
+const Event = async ({
+  searchParams,
+}: {
+  searchParams: { page: string; limit: string; search: string };
+}) => {
+  const searchParamsData = await searchParams;
+  console.log(searchParamsData);
 
-const Event = () => {
+  const page =
+    typeof searchParamsData.page === "string"
+      ? Number(searchParamsData.page)
+      : 1;
+  const limit =
+    typeof searchParamsData.limit === "string"
+      ? Number(searchParamsData.limit)
+      : 10;
+
+  // Extract search term
+  const search =
+    typeof searchParamsData.search === "string"
+      ? searchParamsData.search
+      : undefined;
+
+  // get the events
+  const { isNextPage, totalCount, isPreviousPage, data } = await GetAllEvent({
+    page: page,
+    limit: limit,
+    query: search,
+  });
+
+  // console.log(data);
+
   return (
     <div>
-      <EventContainer />  
+      <EventContainer
+        page={page}
+        isPreviousPage={isPreviousPage}
+        isNextPage={isNextPage}
+        totalCount={totalCount}
+        search={search}
+        events={data}
+      />
     </div>
   );
 };
