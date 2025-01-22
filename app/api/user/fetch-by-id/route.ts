8504ad34c11ef8142a20f73be1e640f195e0f0c8
kebzin <kebbawaiga@gmail.com>
2025-01-22 11:38:27 +0000

    Refactor user fetch API to improve database connection and model import

--- app/api/user/fetch-by-id/route.ts
+++ app/api/user/fetch-by-id/route.ts
@@ -1,8 +1,7 @@
 import { NextResponse } from "next/server";
-
-import connectDB from "@/lib/validation/db";
-import User from "@/lib/models/auth.model";
 import { IUser } from "@/lib/types";
+import { User } from "@/lib/models/auth.model";
+import connectDB from "../../../../lib/db";
 
 // api/user/fetch-by-id
 export async function POST(req: Request) {
