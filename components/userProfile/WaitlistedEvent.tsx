d39839f7db0f0373fc1ba9c085fd6278c156f72d
kebzin <kebbawaiga@gmail.com>
2025-01-25 14:17:39 +0000

    Refactor StaffTable, RegisteredEvents, and WaitlistedEvent components for improved readability and consistency; enforce IUser type in StaffTable

--- components/userProfile/WaitlistedEvent.tsx
+++ components/userProfile/WaitlistedEvent.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Table, TableBody, TableCell, TableRow } from "../ui/table";
 import DynamicTableHeaders from "../common/DynamicTableHeaders";
-import { RegisteredEv } from "@/constants/tablesData";
+// import { RegisteredEv } from "@/constants/tablesData";
 import { EventData } from "@/constants/sampleData";
 import { formatReadableDate } from "@/lib/utils";
 import { Badge } from "../ui/badge";
@@ -10,7 +10,9 @@ const WaitlistedEvent = () => {
   return (
     <div className="mt-5 overflow-x-auto">
       <Table className="w-full table-auto text-sm sm:text-base">
-        <DynamicTableHeaders headers={RegisteredEv} />
+        <DynamicTableHeaders
+          headers={["Event Start", "Event End", "Create at "]}
+        />
 
         <TableBody>
           {EventData.map((item, index) => (
@@ -18,19 +20,19 @@ const WaitlistedEvent = () => {
               <TableCell>{index + 1}</TableCell>
               <TableCell>{item.title}</TableCell>
               <TableCell>
-              <Badge
-                className={
-                  item.status === "canceled"
-                    ? "bg-red-600"
-                    : item.status === "upcoming"
-                    ? "bg-yellow-600"
-                    : item.status === "ongoing"
-                    ? "bg-lime-500"
-                    : "bg-green-600"
-                }
-              >
-                {item.status}
-              </Badge>
+                <Badge
+                  className={
+                    item.status === "canceled"
+                      ? "bg-red-600"
+                      : item.status === "upcoming"
+                      ? "bg-yellow-600"
+                      : item.status === "ongoing"
+                      ? "bg-lime-500"
+                      : "bg-green-600"
+                  }
+                >
+                  {item.status}
+                </Badge>
               </TableCell>
               <TableCell>{formatReadableDate(item.schedule.start)}</TableCell>
               <TableCell>{formatReadableDate(item.schedule.end)}</TableCell>
